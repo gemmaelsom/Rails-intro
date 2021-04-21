7d95e5cac0df224adeb2cdf8850c42c2057b41a6
Gemma <gemma@gemmaelsom.com>
2021-04-21 14:09:11 +1000

    updated app

--- app/controllers/cafe_controller.rb
+++ app/controllers/cafe_controller.rb
@@ -1,9 +1,28 @@
 class CafeController < ApplicationController
+    # IMPOTANT ! ! REMOVE FOR PRODUCTION!
+    skip_before_action :verify_authenticity_token
+
+    @@menu = {
+        latte: 4.00,
+        scone: 5.00,
+        tea: 3.00
+    }
+
     def index
-        @menu = {
-            latte: 4.00,
-            scone: 5.00,
-            tea: 3.00
-        }
+        @menu = @@menu
+        @currency_symbol = '$'
+    end
+
+    def about
+        render json: @@menu
+    end
+
+    def create
+        @@menu[params[:name].to_sym] = params [:price].to_f
+        json: @@menu
+    end
+
+    def show
+        @item = { name: [params[:item_name], price: @@menu[params[:item_name].to_sym]]}
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "cafe#index"
+  root to: 'cafe#index'
+  get '/cafe', to: 'cafe#about'
+  post '/cafe', to: 'cafe#create'
+  get '/cafe/:item_name', to: 'cafe#show'
 end
